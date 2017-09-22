0e5f644917b4ff4bc3ef212f4130d3f04dcd080f
Yifan Hong <elsk@google.com>
2017-09-22 15:16:18 -0700

    clover: uprev to health@2.0 hal.
    Test: adb logcat -e health
    Test: lshal --debug
    
    Change-Id: I3b14f087d95578d034898600592cb6fb357a9958

--- device.mk
+++ device.mk
@@ -220,8 +220,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
