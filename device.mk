304ce3280e4d7268f0f2b115ac50bf5c8368913f
guaiyihu <guaiyihu@foxmail.com>
2018-07-10 18:09:30 +0800

    clover: device: Correct boot animation size
    Change-Id: I8ee8187935474ba3d2e55f4e8d844ac6614f27b9

--- device.mk
+++ device.mk
@@ -74,8 +74,8 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2160
-TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1200
 
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/whitelistedapps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/whitelistedapps.xml \
                       $(LOCAL_PATH)/configs/gamedwhitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gamedwhitelist.xml \
