a2f7e5a3427f3ce4d3b2c0613dc7430dce397631
guaiyihu <guaiyihu@foxmail.com>
2018-07-10 18:08:40 +0800

    clover: device: Clean up unsupported vibrator blobs
    Change-Id: I3feb6f4c27fd319901a343989c8c60746ada6011

--- device.mk
+++ device.mk
@@ -332,11 +332,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service \
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
