ffdad184b3a184d42ee8cf203f8c31089dd20ad4
guaiyihu <guaiyihu@foxmail.com>
2018-09-17 11:55:46 +0800

    clover: device: Build vendor.qti.hardware.camera.device@1.0
    Change-Id: I898fe0b0252682e2396a02454d851e04a821f6ec

--- device.mk
+++ device.mk
@@ -145,6 +145,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
     libxml2 \
     Snap
 
