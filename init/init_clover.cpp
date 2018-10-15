c3104ac98fb8ed126c1ac5485b9d5c78909a0d8e
dianlujitao <dianlujitao@lineageos.org>
2018-10-15 16:06:26 +0800

    clover: Remove power-off alarm stuff from init extension
     * CAF's init.qcom.early_boot.sh checks /persist/alarm/powerOffAlarmSet
       before setting ro.alarm_boot, but on Oreo nothing writes to that
       file, thus code is never executed and the prop is never set.
     * Without ro.alarm_boot=true, the system will boot normally instead of
       boot to min framework even on rtc, and alarm will ring after boot
       completed.
    
    Change-Id: Ie4d30358314337c229a66a16195f675198ef7d52

--- init/init_clover.cpp
+++ init/init_clover.cpp
@@ -26,13 +26,11 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <fstream>
 #include <unistd.h>
 
 #include <android-base/file.h>
 #include <android-base/properties.h>
-#include <android-base/strings.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
@@ -41,7 +39,6 @@
 
 using android::base::GetProperty;
 using android::base::ReadFileToString;
-using android::base::Trim;
 using android::init::property_set;
 
 void property_override(char const prop[], char const value[])
@@ -60,40 +57,6 @@ void property_override_dual(char const system_prop[],
 {
     property_override(system_prop, value);
     property_override(vendor_prop, value);
-}
-
-static void init_alarm_boot_properties()
-{
-    char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
-    char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
-    std::string boot_reason;
-    std::string power_off_alarm;
-    std::string reboot_reason = GetProperty("ro.boot.alarmboot", "");
-
-    if (ReadFileToString(boot_reason_file, &boot_reason)
-            && ReadFileToString(power_off_alarm_file, &power_off_alarm)) {
-        /*
-         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
-         * For existing PMIC chips, the following mapping applies
-         * for the value of boot_reason:
-         *
-         * 0 -> unknown
-         * 1 -> hard reset
-         * 2 -> sudden momentary power loss (SMPL)
-         * 3 -> real time clock (RTC)
-         * 4 -> DC charger inserted
-         * 5 -> USB charger inserted
-         * 6 -> PON1 pin toggled (for secondary PMICs)
-         * 7 -> CBLPWR_N pin toggled (for external power supply)
-         * 8 -> KPDPWR_N pin toggled (power key pressed)
-         */
-         if ((Trim(boot_reason) == "3" || reboot_reason == "true")
-                 && Trim(power_off_alarm) == "1") {
-             property_set("ro.alarm_boot", "true");
-         } else {
-             property_set("ro.alarm_boot", "false");
-         }
-    }
 }
 
 void vendor_load_properties()
@@ -120,5 +83,4 @@ void vendor_load_properties()
         property_override_dual("ro.product.model", "ro.vendor.product.model", "MI PAD 4");
     }
 
-    init_alarm_boot_properties();
 }
