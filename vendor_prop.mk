e2fea538206361b876d9c852689b89211377116b
dianlujitao <dianlujitao@lineageos.org>
2019-02-22 23:36:41 +0800

    clover: Add instagram to HAL1 list
    Change-Id: I6006fdd7bf7df530238827fc2e488ee47c72fbd8

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -77,7 +77,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # HAL1 apps list
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner
+    vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner,com.instagram.android
 
 # Debug Options
 PRODUCT_PROPERTY_OVERRIDES += \
