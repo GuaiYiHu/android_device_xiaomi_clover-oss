d04a6a8be1ed8b7b0d5f48e4466537f9c418c391
GuaiYiHu <guaiyihu@foxmail.com>
2019-03-04 09:38:49 +0800

    clover: bluetooth: Set bluetooth name dynamically
    Change-Id: Id9c7d5a51373f4513482c2d6ae262c8481b94575

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -21,7 +21,26 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
-#define BTM_DEF_LOCAL_NAME   "MI PAD 4"
+
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char* BtmGetDefaultName()
+{
+    char product_model[PROPERTY_VALUE_MAX];
+    property_get("ro.product.model", product_model, "");
+
+    if (strstr(product_model, "MI PAD 4"))
+        return "MI PAD 4";
+    if (strstr(product_model, "MI PAD 4 PLUS"))
+        return "MI PAD 4 PLUS";
+
+    // Fallback to ro.product.model
+    return "";
+}
+#undef PROPERTY_VALUE_MAX
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 #define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
