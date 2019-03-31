6dc484c0f711c27a2343460aa8dc6d8e10f531ab
GuaiYiHu <guaiyihu@foxmail.com>
2019-03-31 12:45:04 +0800

    clover: Drop MiuiCamera
    Change-Id: I2ad7c7754f22e57bfc4f5ef4bfc1312581557a2b

--- mk_clover.mk
+++ mk_clover.mk
@@ -18,7 +18,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
-$(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/clover.mk)
 
 # Inherit from clover device
 $(call inherit-product, device/xiaomi/clover/device.mk)
