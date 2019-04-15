a3bc81238c8e550f36eb645ca02c0c2140372cb5
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    clover: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- lineage_clover.mk
+++ lineage_clover.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit from clover device
 $(call inherit-product, device/xiaomi/clover/device.mk)
 
