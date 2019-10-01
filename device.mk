8fa367109f976d6309e0a5d31fcf086cf2f64f26
GuaiYiHu <guaiyihu@foxmail.com>
2019-10-01 16:15:42 +0800

    clover: Kang folio_daemon from wahoo
    Change-Id: Ib10db4a2889c9571b442b96c8b29326a492655bd

--- device.mk
+++ device.mk
@@ -186,6 +186,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# Folio
+PRODUCT_PACKAGES += \
+    folio_daemon
+
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
---
+++ folio_daemon/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES := \
+    libandroid \
+    libcutils \
+    liblog
+
+LOCAL_SRC_FILES := \
+    main.cpp
+
+LOCAL_C_INCLUDES :=
+
+LOCAL_CFLAGS := $(common_flags) -DLOG_TAG=\"folio_daemon\" -DLOG_NDEBUG=0
+
+LOCAL_CFLAGS += -Wall -Werror
+
+LOCAL_MODULE := folio_daemon
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_OWNER := google
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
+
+include $(BUILD_EXECUTABLE)
